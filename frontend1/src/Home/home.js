9ebf23535290a19e3794e8cc990e3d2558c9b78a
achyuta22 <136910474+achyuta22@users.noreply.github.com>
2024-03-08 01:20:19 +0530

    Update home.js

--- frontend1/src/Home/home.js
+++ frontend1/src/Home/home.js
@@ -43,7 +43,7 @@ const Home = () => {
             </h1>
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
               From checkout to global sales tax compliance, companies around the
-              world use Flowbite to simplify their payment stack.
+              world use Post Link to simplify their payment stack.
             </p>
             <a
               href="#"
